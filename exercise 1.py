ecbb253966383a8dd26be25c003b1f56ed7259d9
ASDenne <98923598+ASDenne@users.noreply.github.com>
2022-03-16 13:50:31 +1300

    Update exercise 1.py

--- exercise 1.py
+++ exercise 1.py
@@ -1,29 +1,39 @@
 def drop_off():
-    print()
+    Dogs.append(input("whats thier name"))
+
 def pick_up():
-    print()
+    leaving = input("whats thier name")
+    if leaving in Dogs:
+        Dogs.remove(leaving)
+        print(leaving+"removed")
+    else:
+        print("not in register")
 def Printroll():
-    print()
+    print(Dogs)
 def calccost():
-    print()
+    print(len(Dogs)*22.5)
 #main
 Dogs = []
+
 control = 0
-while control != 4:
+Dogs
+while control != 5:
     print("what do you want to do?")
     print("1 drop a pet off")
     print("2 pick a pet up")
     print("3 print roll")
-    print("4 quit")
+    print("4 money made today")
+    print("5 shut down")
     control = int(input(">>"))
-    while control > 0 and control < 5:
+    while control > 6 and control < 0:
         print("please enter a number between 1 and 4")
         control = int(input(">>"))
     if control == 1:
-        print(1)
+        drop_off()
     elif control == 2:
-        print(2)
+        pick_up()
     elif control == 3:
-        print(3)
-    else:
-        print(4)
+        Printroll()
+    elif control == 4:
+        calccost()
+
