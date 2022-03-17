b7dabdcd81ccc1f1bc09d1a822b1d8c5f520cbbc
ASDenne <98923598+ASDenne@users.noreply.github.com>
2022-03-17 14:40:41 +1300

    Update exercise 3.py

--- exercise 3.py
+++ exercise 3.py
@@ -1,8 +1,60 @@
 import random
+def name():
+    return input("who are you")
 def player_guess():
-    print()
+    print("paper siccors rock")
+    guess = input(">>>").lower()
+    if guess == "paper":
+        return 1
+    elif guess == "rock":
+        return 2
+    else:
+        return 3
 def computer_guess():
-    print()
+    return random.randint(1,3)
 def who_won(Pguess,Cguess):
-    print()
-def 
+    if Pguess == Cguess:
+        print("this oround was a draw")
+        return "n"
+    elif Pguess == 3:
+        if Cguess == 1:
+            print("you lost this round")
+            return "c"
+        else:
+            print("you won this round")
+            return "p"
+
+    elif Cguess == 3:
+        if Pguess == 1:
+            print("this round winner is you")
+            return "p"
+        else:
+            print("you lost to the computer this round")
+            return "c"
+    elif Cguess > Pguess:
+        print("you lost to a robot this round")
+        return  "c"
+    else:
+        print("you win this round")
+        return "p"
+
+
+
+#main
+Player = name()
+Pguess = ""
+Cguess = ""
+Pscore = 0
+Cscore = 0
+while Cscore + Pscore != 3:
+    Pguess = player_guess()
+    Cguess = computer_guess()
+    Rwinner = who_won(Pguess, Cguess)
+    if Rwinner == "c":
+        Cscore += 1
+    elif Pscore == "p":
+        Pscore += 1
+if Cscore > Pscore:
+    print(f"{Player} lost")
+else:
+    print(f"y{Player} won")
